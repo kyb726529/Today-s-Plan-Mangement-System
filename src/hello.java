d9f52c3e491bf07993a64c504dd9b8d096d0f486
Koo <kyb726529@naver.com>
2021-03-21 23:19:22 +0900

    added hello.java and menumanager.java

--- src/hello.java
+++ src/hello.java
@@ -2,7 +2,12 @@
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("1. Add Studnet");
+		System.out.println("2. Delete Student");
+		System.out.println("3. Edit Student");
+		System.out.println("4. View Student");
+		System.out.println("Exit");
+
 	}
 
 }
