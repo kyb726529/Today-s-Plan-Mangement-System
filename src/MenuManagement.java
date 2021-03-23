156d10e278f21cfd1a6563e42fd434b90d4db31d
Koo <kyb726529@naver.com>
2021-03-23 15:39:21 +0900

    add menu selection 1

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -7,18 +7,18 @@ public class MenuManagement {
 		Scanner input=new Scanner(System.in);
 		while(num!=6) {
 		
-		System.out.println("1. Add Studnet");
-		System.out.println("2. Delete Student");
-		System.out.println("3. Edit Student");
-		System.out.println("4. View Student");
+		System.out.println("1. Add Plan");
+		System.out.println("2. Delete Plan");
+		System.out.println("3. Edit Plan");
+		System.out.println("4. View Plan");
 		System.out.println("Exit");
 		System.out.println("Slect one number between 1-6");
 		num=input.nextInt();
 		if(num==1) {
-			System.out.println("Student Id :");
-			int StudentId=input.nextInt();
-			System.out.println("Student Name :");
-			String StudentName=input.next();
+			System.out.println("What to do :");
+			String NewPlan=input.next();
+			System.out.println("When to do :");
+			int Priority=input.nextInt();
 			}
 		}
 
