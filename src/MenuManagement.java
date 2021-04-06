05789ba45a1c17e1ac601c0b82e867d1faeddd69
Koo <kyb726529@naver.com>
2021-04-06 16:23:04 +0900

    Xmanagement with class

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -3,24 +3,35 @@ import java.util.Scanner;
 public class MenuManagement {
 
 	public static void main(String[] args) {
-		int num=0;
+		
 		Scanner input=new Scanner(System.in);
+		PlanManager planManager=new PlanManager(input);
+		int num=0;
 		while(num!=6) {
 		
-		System.out.println("1. Add Plan");
-		System.out.println("2. Delete Plan");
-		System.out.println("3. Edit Plan");
-		System.out.println("4. View Plan");
-		System.out.println("Exit");
-		System.out.println("Slect one number between 1-6");
-		num=input.nextInt();
-		if(num==1) {
-			System.out.println("What to do :");
-			String NewPlan=input.next();
-			System.out.println("When to do :");
-			int Priority=input.nextInt();
+			System.out.println("1. Add Plan");
+			System.out.println("2. Delete Plan");
+			System.out.println("3. Edit Plan");
+			System.out.println("4. View Plan");
+			System.out.println("5. Exit");
+			System.out.println("Slect one number between 1-6");
+			num=input.nextInt();
+			if (num==1) {
+				planManager.addplan();
+			}
+			else if(num==2) {
+				planManager.deleteplan();
+			}
+			else if(num==3) {
+				planManager.editplan();
+			}
+			else if(num==4) {
+				planManager.viewplan();
+			}
+			else {
+				continue;
 			}
 		}
-
 	}
+	
 }
